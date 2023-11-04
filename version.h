0afc392b530f6e60bd90e729c2fb546f001e4b9b
Sebastião Teixeira <steixeira2004@gmail.com>
2023-11-04 15:48:03 +0000

    First Commit

---
+++ version.h
@@ -0,0 +1,30 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+	//Date Version Types
+	static const char DATE[] = "00";
+	static const char MONTH[] = "00";
+	static const char YEAR[] = "0000";
+	static const char UBUNTU_VERSION_STYLE[] =  "00.00";
+	
+	//Software Status
+	static const char STATUS[] =  "Alpha";
+	static const char STATUS_SHORT[] =  "a";
+	
+	//Standard Version Type
+	static const long MAJOR  = 0;
+	static const long MINOR  = 0;
+	static const long BUILD  = 0;
+	static const long REVISION  = 0;
+	
+	//Miscellaneous Version Types
+	static const long BUILDS_COUNT  = 0;
+	#define RC_FILEVERSION 0,0,0,0
+	#define RC_FILEVERSION_STRING "0, 0, 0, 0\0"
+	static const char FULLVERSION_STRING [] = "0.0.0.0";
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	static const long BUILD_HISTORY  = 35;
+	
+
+#endif //VERSION_H
